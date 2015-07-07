016a0ca4e91f23efabf39c185a559641286f3784
Brad Ballard <brad@bradballard.com>
2015-07-07 19:01:49 -0400

    Fix double quotes and change to single quotes

--- parser.rb
+++ parser.rb
@@ -7,22 +7,22 @@ MISSING_INITIAL = /[POpo]/
 original_data = Array.new
 converted_data = Array.new
     
-Dir.chdir "convert"
-File.foreach("DEM150601.DHA") do |raw_file|
-  original_data << raw_file.split()
+Dir.chdir 'convert'
+File.foreach('DEM150601.DHA') do |raw_file|
+  original_data << raw_file.split
 end
 
-converted_data.insert(0, "Acvite", "ACT")
+converted_data.insert(0, 'Acvite', 'ACT')
 
 original_data.each do |o|
   converted_data << o.slice(0)
 
-  patient_name = ""
-  initial = " "
-  patient_name << o.slice(2..3).join(" ")
+  patient_name = ''
+  initial = ' '
+  patient_name << o.slice(2..3).join(' ')
 
   if o.slice(4).match(MISSING_INITIAL)
-    initial ||= ""
+    initial ||= ''
   else o.slice(4).match(ALPHA)
     initial << o.slice(4)
   end
@@ -31,7 +31,7 @@ original_data.each do |o|
 end
 
 converted_data[2].slice!(0..1)
-if converted_data[2].slice(1) == "0"
+if converted_data[2].slice(1) == '0'
   converted_data[2].slice!(1)
 end
 
